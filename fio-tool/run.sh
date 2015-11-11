907c0764cdc2212af5832199c9cdee23da847dfb
Ryan Wallner <wallnerryan@users.noreply.github.com>
2015-11-11 12:21:51 -0800

    Update run.sh
    fix fio-tool bug where it would nuke your jobfile gets nukes if you dont want it to

--- fio-tool/run.sh
+++ fio-tool/run.sh
@@ -3,10 +3,14 @@
 [ -z "$JOBFILES" ] && echo "Need to set JOBFILES" && exit 1;
 echo "Running $JOBFILES"
 
-# We really want no old data in here
+# We really want no old data in here except the fio script
+mv /tmp/fio-data/*.fio /tmp/
 rm -rf /tmp/fio-data/*
+mv /tmp/*fio /tmp/fio-data/
 
 if [ ! -z "$REMOTEFILES" ]; then
+    # We really want no old data in here
+    rm -rf /tmp/fio-data/*
     IFS=' '
     echo "Gathering remote files..."
     for file in $REMOTEFILES; do
